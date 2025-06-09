d7e108a5c11922575a9b3edbc1afb137f9057956
Daniele Canzoneri <danielecanzoneri@gmail.com>
2025-06-09 13:55:49 +0200

    Rendering optimization (from 70% of time spent on ebiten.Draw to 45%)

--- emulator/ui/render.go
+++ emulator/ui/render.go
@@ -1,10 +1,11 @@
 package ui
 
 import (
+	"image/color"
+
 	"github.com/danielecanzoneri/gb-emulator/emulator/internal/gameboy/ppu"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"image/color"
 )
 
 const (
@@ -12,12 +13,6 @@ const (
 )
 
 var (
-	//colors = [4]color.Color{
-	//	color.Gray{Y: 0xFF}, // White
-	//	color.Gray{Y: 0xAA}, // Light gray
-	//	color.Gray{Y: 0x55}, // Dark gray
-	//	color.Gray{Y: 0},    // Black
-	//}
 	// Original palette
 	colors = [4]color.Color{
 		color.RGBA{R: 198, G: 222, B: 140, A: 255},
@@ -25,17 +20,12 @@ var (
 		color.RGBA{R: 57, G: 97, B: 57, A: 255},
 		color.RGBA{R: 8, G: 24, B: 16, A: 255},
 	}
-	pixels [4]*ebiten.Image
+	frameImage *ebiten.Image
 )
 
 func initRenderer() {
-	for i := range pixels {
-		// Create a Scale x Scale image of the corresponding color
-		square := ebiten.NewImage(Scale, Scale)
-		square.Fill(colors[i])
-
-		pixels[i] = square
-	}
+	// Create a single image for the entire frame
+	frameImage = ebiten.NewImage(ppu.FrameWidth, ppu.FrameHeight)
 }
 
 // Inherit Ebiten Game interface
@@ -60,22 +50,18 @@ func (ui *UI) Update() error {
 }
 
 func (ui *UI) Draw(screen *ebiten.Image) {
-	// Background color
-	screen.Fill(color.RGBA{R: 40, G: 40, B: 40, A: 220})
-
-	// Draw all 144 x 160 pixels
-	gameScreen := ebiten.NewImage(ui.Layout(0, 0))
+	// Update the frame image with the current framebuffer
 	for y := range ppu.FrameHeight {
 		for x := range ppu.FrameWidth {
-			op := &ebiten.DrawImageOptions{}
-			op.GeoM.Translate(float64(Scale*x), float64(Scale*y))
-
 			colorId := ui.gameBoy.PPU.Framebuffer[y][x]
-			gameScreen.DrawImage(pixels[colorId], op)
+			frameImage.Set(x, y, colors[colorId])
 		}
 	}
 
-	screen.DrawImage(gameScreen, nil)
+	// Draw the entire frame at once with scaling
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(Scale, Scale)
+	screen.DrawImage(frameImage, op)
 
 	if ui.debugStringTimer > 0 {
 		ebitenutil.DebugPrint(screen, ui.debugString)
