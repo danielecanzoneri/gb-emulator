a1f9c51d04474dad9a3efe97ecb04a03ea2ed77f
Daniele Canzoneri <danielecanzoneri@gmail.com>
2025-07-07 10:29:20 +0200

    Separate panel logic

--- emulator/ui/debugger/registersViewer.go
+++ emulator/ui/debugger/registersViewer.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/danielecanzoneri/gb-emulator/emulator/internal/gameboy"
 	"github.com/ebitenui/ebitenui/widget"
-	"golang.org/x/image/colornames"
 )
 
 type registersViewer struct {
@@ -19,7 +18,7 @@ type registersViewer struct {
 	//waveRam *waveRamPanel
 
 	// CPU
-	cpu *cpuPanel
+	cpu *panel
 	//interrupts *interruptsPanel
 
 	//lcd   *lcdPanel
@@ -56,64 +55,27 @@ func (v *registersViewer) Sync(gb *gameboy.GameBoy) {
 	//v.timer.Sync(gb)
 }
 
-type cpuPanel struct {
-	*widget.Container
+func newCpuPanel() *panel {
+	entries := []panelEntry{
+		{name: "AF", valueSync: func(gb *gameboy.GameBoy) string {
+			return fmt.Sprintf("%04X", gb.CPU.ReadAF())
+		}},
+		{name: "BC", valueSync: func(gb *gameboy.GameBoy) string {
+			return fmt.Sprintf("%04X", gb.CPU.ReadBC())
+		}},
+		{name: "DE", valueSync: func(gb *gameboy.GameBoy) string {
+			return fmt.Sprintf("%04X", gb.CPU.ReadDE())
+		}},
+		{name: "HL", valueSync: func(gb *gameboy.GameBoy) string {
+			return fmt.Sprintf("%04X", gb.CPU.ReadHL())
+		}},
+		{name: "PC", valueSync: func(gb *gameboy.GameBoy) string {
+			return fmt.Sprintf("%04X", gb.CPU.ReadPC())
+		}},
+		{name: "SP", valueSync: func(gb *gameboy.GameBoy) string {
+			return fmt.Sprintf("%04X", gb.CPU.ReadSP())
+		}},
+	}
 
-	labelAF *widget.Text
-	labelBC *widget.Text
-	labelDE *widget.Text
-	labelHL *widget.Text
-	labelPC *widget.Text
-	labelSP *widget.Text
-}
-
-func newCpuPanel() *cpuPanel {
-	p := new(cpuPanel)
-
-	// Create container
-	p.Container = newContainer(widget.DirectionVertical)
-
-	// Panel title
-	titleLabel := newLabel("CPU", colornames.Yellow)
-	p.AddChild(titleLabel)
-
-	// Two vertical containers: one with labels and one with values
-	labels := newContainer(widget.DirectionVertical,
-		newLabel("AF", colornames.White),
-		newLabel("BC", colornames.White),
-		newLabel("DE", colornames.White),
-		newLabel("HL", colornames.White),
-		newLabel("PC", colornames.White),
-		newLabel("SP", colornames.White),
-	)
-
-	p.labelAF = newLabel("", colornames.White)
-	p.labelBC = newLabel("", colornames.White)
-	p.labelDE = newLabel("", colornames.White)
-	p.labelHL = newLabel("", colornames.White)
-	p.labelPC = newLabel("", colornames.White)
-	p.labelSP = newLabel("", colornames.White)
-	values := newContainer(widget.DirectionVertical,
-		p.labelAF, p.labelBC, p.labelDE, p.labelHL, p.labelPC, p.labelSP,
-	)
-
-	core := widget.NewContainer(widget.ContainerOpts.Layout(
-		widget.NewRowLayout(
-			widget.RowLayoutOpts.Direction(widget.DirectionHorizontal),
-			widget.RowLayoutOpts.Spacing(10),
-		),
-	))
-	core.AddChild(labels, values)
-	p.AddChild(core)
-
-	return p
-}
-
-func (p *cpuPanel) Sync(gb *gameboy.GameBoy) {
-	p.labelAF.Label = fmt.Sprintf("%04X", gb.CPU.ReadAF())
-	p.labelBC.Label = fmt.Sprintf("%04X", gb.CPU.ReadBC())
-	p.labelDE.Label = fmt.Sprintf("%04X", gb.CPU.ReadDE())
-	p.labelHL.Label = fmt.Sprintf("%04X", gb.CPU.ReadHL())
-	p.labelPC.Label = fmt.Sprintf("%04X", gb.CPU.ReadPC())
-	p.labelSP.Label = fmt.Sprintf("%04X", gb.CPU.ReadSP())
+	return newPanel("CPU", entries...)
 }
