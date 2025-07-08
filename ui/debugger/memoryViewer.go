323b96c307df575448bd2afa6c4a9df4e19ae38a
Daniele Canzoneri <danielecanzoneri@gmail.com>
2025-07-08 09:49:20 +0200

    Add ascii view of memory

--- ui/debugger/memoryViewer.go
+++ ui/debugger/memoryViewer.go
@@ -2,11 +2,12 @@ package debugger
 
 import (
 	"fmt"
+	"image/color"
+
 	"github.com/danielecanzoneri/gb-emulator/gameboy"
 	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 	"golang.org/x/image/colornames"
-	"image/color"
 )
 
 // disassemblyEntry represents a single line in the disassembler
@@ -110,7 +111,7 @@ func (mv *memoryViewer) Sync(gb *gameboy.GameBoy) {
 }
 
 func (mv *memoryViewer) createRow() widget.PreferredSizeLocateableWidget {
-	dummyText := "0000  00 00 00 00 00 00 00 00 | 00 00 00 00 00 00 00 00"
+	dummyText := "0000  00 00 00 00 00 00 00 00 | 00 00 00 00 00 00 00 00 | ................"
 	label := widget.NewText(
 		widget.TextOpts.Text(dummyText, font, colornames.White), // Font and text
 	)
@@ -122,12 +123,21 @@ func (mv *memoryViewer) refresh() {
 	for i, r := range mv.rowsWidget {
 		label := r.(*widget.Text)
 		entry := mv.entries[mv.first+i]
-		label.Label = fmt.Sprintf("%04X  %02X %02X %02X %02X %02X %02X %02X %02X | %02X %02X %02X %02X %02X %02X %02X %02X",
+		ascii := make([]byte, 16)
+		for i, b := range entry.data {
+			if b >= 32 && b <= 126 {
+				ascii[i] = b
+			} else {
+				ascii[i] = '.'
+			}
+		}
+		label.Label = fmt.Sprintf("%04X  %02X %02X %02X %02X %02X %02X %02X %02X | %02X %02X %02X %02X %02X %02X %02X %02X | %s",
 			entry.baseAddress,
 			entry.data[0], entry.data[1], entry.data[2], entry.data[3],
 			entry.data[4], entry.data[5], entry.data[6], entry.data[7],
 			entry.data[8], entry.data[9], entry.data[10], entry.data[11],
 			entry.data[12], entry.data[13], entry.data[14], entry.data[15],
+			string(ascii),
 		)
 	}
 }
