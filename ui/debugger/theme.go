89468f1270b28a0e1205507e3e2f4dfb4539032b
Daniele Canzoneri <danielecanzoneri@gmail.com>
2025-07-10 07:08:48 +0200

    Improved UI colors

--- ui/debugger/theme.go
+++ ui/debugger/theme.go
@@ -1,47 +1,56 @@
 package debugger
 
 import (
+	"image/color"
+
 	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
-	"image/color"
 )
 
 var (
-	backgroundColor  = color.NRGBA{R: 24, G: 28, B: 38, A: 255}    // very dark blue-gray
-	labelColor       = color.NRGBA{R: 220, G: 220, B: 230, A: 255} // light gray for text
-	titleColor       = color.NRGBA{R: 255, G: 214, B: 102, A: 255} // soft pastel yellow
-	buttonLabelColor = color.NRGBA{R: 230, G: 230, B: 240, A: 255} // light gray for button text
+	// General Background and Text
+	backgroundColor = color.NRGBA{R: 20, G: 22, B: 30, A: 255}    // nearly black-blue
+	labelColor      = color.NRGBA{R: 220, G: 220, B: 230, A: 255} // light gray for readability
+	titleColor      = color.NRGBA{R: 255, G: 200, B: 80, A: 255}  // soft warm yellow
 
-	buttonColor        = color.NRGBA{R: 104, G: 129, B: 159, A: 255} // lighter muted blue
-	buttonHoverColor   = color.NRGBA{R: 134, G: 159, B: 199, A: 255} // even lighter muted blue
-	buttonPressedColor = color.NRGBA{R: 164, G: 189, B: 239, A: 255} // lightest muted blue
+	// Buttons
+	buttonColor        = color.NRGBA{R: 60, G: 90, B: 130, A: 255}   // desaturated blue
+	buttonHoverColor   = color.NRGBA{R: 80, G: 110, B: 160, A: 255}  // brighter on hover
+	buttonPressedColor = color.NRGBA{R: 100, G: 130, B: 190, A: 255} // even brighter
+	buttonLabelColor   = color.NRGBA{R: 235, G: 235, B: 245, A: 255} // high contrast
 
-	mainColor        = color.NRGBA{R: 44, G: 34, B: 54, A: 255} // dark muted purple
-	mainHoverColor   = color.NRGBA{R: 64, G: 54, B: 74, A: 255} // lighter muted purple
-	mainPressedColor = color.NRGBA{R: 84, G: 74, B: 94, A: 255} // even lighter muted purple
+	// Main UI Panels / Containers
+	mainColor        = color.NRGBA{R: 35, G: 30, B: 45, A: 255} // deep purple-gray
+	mainHoverColor   = color.NRGBA{R: 50, G: 45, B: 65, A: 255} // slightly lighter
+	mainPressedColor = color.NRGBA{R: 65, G: 60, B: 85, A: 255} // more lifted
 
-	breakpointColor        = color.NRGBA{R: 120, G: 60, B: 60, A: 255}   // muted dark red
-	breakpointHoverColor   = color.NRGBA{R: 160, G: 80, B: 80, A: 255}   // lighter muted red
-	breakpointPressedColor = color.NRGBA{R: 200, G: 100, B: 100, A: 255} // even lighter muted red
+	// Breakpoints
+	breakpointColor        = color.NRGBA{R: 140, G: 40, B: 40, A: 255} // dark red
+	breakpointHoverColor   = color.NRGBA{R: 170, G: 60, B: 60, A: 255} // hover pop
+	breakpointPressedColor = color.NRGBA{R: 200, G: 80, B: 80, A: 255} // more vivid
 
-	currInstrColor        = color.NRGBA{R: 60, G: 120, B: 100, A: 255}  // muted dark teal
-	currInstrHoverColor   = color.NRGBA{R: 80, G: 160, B: 130, A: 255}  // lighter muted teal
-	currInstrPressedColor = color.NRGBA{R: 100, G: 200, B: 160, A: 255} // even lighter muted teal
+	// Current Instruction
+	currInstrColor        = color.NRGBA{R: 40, G: 120, B: 110, A: 255} // dark teal
+	currInstrHoverColor   = color.NRGBA{R: 60, G: 150, B: 140, A: 255} // hover highlight
+	currInstrPressedColor = color.NRGBA{R: 80, G: 180, B: 170, A: 255} // active highlight
 
-	breakpointCurrColor        = color.NRGBA{R: 180, G: 80, B: 180, A: 255} // vibrant violet
-	breakpointCurrHoverColor   = color.NRGBA{R: 200, G: 120, B: 200, A: 255}
-	breakpointCurrPressedColor = color.NRGBA{R: 220, G: 160, B: 220, A: 255}
+	// Breakpoint + Current Instruction
+	breakpointCurrColor        = color.NRGBA{R: 160, G: 60, B: 160, A: 255} // magenta
+	breakpointCurrHoverColor   = color.NRGBA{R: 180, G: 100, B: 180, A: 255}
+	breakpointCurrPressedColor = color.NRGBA{R: 200, G: 140, B: 200, A: 255}
 
-	toolbarBackgroundColor   = color.NRGBA{R: 12, G: 14, B: 90, A: 255}
-	toolbarTextColor         = color.White
-	toolbarMenuColor         = color.NRGBA{R: 12, G: 14, B: 19, A: 255}
-	toolbarMenuHoverColor    = color.NRGBA{R: 12, G: 14, B: 19, A: 255}
-	toolbarMenuPressedColor  = color.NRGBA{R: 12, G: 14, B: 19, A: 255}
-	toolbarEntryColor        = color.NRGBA{R: 12, G: 14, B: 19, A: 255}
-	toolbarEntryHoverColor   = color.NRGBA{R: 12, G: 14, B: 19, A: 255}
-	toolbarEntryPressedColor = color.NRGBA{R: 12, G: 14, B: 19, A: 255}
+	// Toolbar
+	toolbarBackgroundColor   = color.NRGBA{R: 18, G: 20, B: 30, A: 255} // matches general bg
+	toolbarTextColor         = color.NRGBA{R: 230, G: 230, B: 240, A: 255}
+	toolbarMenuColor         = color.NRGBA{R: 30, G: 30, B: 40, A: 255}
+	toolbarMenuHoverColor    = color.NRGBA{R: 40, G: 40, B: 50, A: 255}
+	toolbarMenuPressedColor  = color.NRGBA{R: 50, G: 50, B: 60, A: 255}
+	toolbarEntryColor        = toolbarMenuColor
+	toolbarEntryHoverColor   = toolbarMenuHoverColor
+	toolbarEntryPressedColor = toolbarMenuPressedColor
 
-	sliderTrackColor = color.NRGBA{R: 255, G: 255, B: 255, A: 32}
+	// Slider
+	sliderTrackColor = color.NRGBA{R: 255, G: 255, B: 255, A: 48} // semi-transparent white
 )
 
 var (
