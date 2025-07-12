226e766acb853b56e84fafb27d44c9264c8d02da
Daniele Canzoneri <danielecanzoneri@gmail.com>
2025-07-12 19:37:37 +0200

    Uniform Joypad function receiver naming

--- gameboy/joypad/joypad.go
+++ gameboy/joypad/joypad.go
@@ -14,12 +14,12 @@ func New() *Joypad {
 	return &Joypad{startDown: 1, selectUp: 1, bLeft: 1, aRight: 1}
 }
 
-func (j *Joypad) Write(v uint8) {
-	j.selectButtons = (v & 0x20) >> 5
-	j.selectDPad = (v & 0x10) >> 4
+func (jp *Joypad) Write(v uint8) {
+	jp.selectButtons = (v & 0x20) >> 5
+	jp.selectDPad = (v & 0x10) >> 4
 }
 
-func (j *Joypad) Read() uint8 {
-	return 0xC0 | (j.selectButtons << 5) | (j.selectDPad << 4) |
-		(j.startDown << 3) | (j.selectUp << 2) | (j.bLeft << 1) | j.aRight
+func (jp *Joypad) Read() uint8 {
+	return 0xC0 | (jp.selectButtons << 5) | (jp.selectDPad << 4) |
+		(jp.startDown << 3) | (jp.selectUp << 2) | (jp.bLeft << 1) | jp.aRight
 }
