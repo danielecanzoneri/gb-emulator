0fac759e2a1b638898de19c77d93c1881ac6d08e
Daniele Canzoneri <danielecanzoneri@gmail.com>
2025-07-24 11:07:29 +0200

    Fix OAM and vRAM access after PPU enabled

--- gameboy/ppu/ppuStatesMode2.go
+++ gameboy/ppu/ppuStatesMode2.go
@@ -1,8 +1,6 @@
 package ppu
 
-import (
-	"github.com/danielecanzoneri/gb-emulator/util"
-)
+import "github.com/danielecanzoneri/gb-emulator/util"
 
 // When PPU is enabled:
 //   - line 0 starts with mode 0 and goes straight to mode 3
@@ -22,6 +20,22 @@ func (st *glitchedMode2) Next(_ *PPU) ppuInternalState {
 }
 func (st *glitchedMode2) Duration() int { return mode2Length }
 
+// ------- Normal mode 2 -------
+
+// Mode 2 first 4 ticks
+type mode0ToMode2 struct {
+}
+
+func (st *mode0ToMode2) Init(ppu *PPU) {
+	util.SetBit(&ppu.STAT, 2, 0)
+
+	ppu.oam.readDisabled = true
+}
+func (st *mode0ToMode2) Next(_ *PPU) ppuInternalState {
+	return new(mode2)
+}
+func (st *mode0ToMode2) Duration() int { return 4 }
+
 // Normal mode 2
 type mode2 struct{}
 
@@ -35,18 +49,17 @@ func (st *mode2) Init(ppu *PPU) {
 	ppu.searchOAM()
 }
 func (st *mode2) Next(_ *PPU) ppuInternalState {
+	return new(mode2ToMode3)
+}
+func (st *mode2) Duration() int { return mode2Length - 4 }
+
+type mode2ToMode3 struct{}
+
+func (st *mode2ToMode3) Init(ppu *PPU) {
+	ppu.oam.writeDisabled = false
+	ppu.vRAM.readDisabled = true
+}
+func (st *mode2ToMode3) Next(_ *PPU) ppuInternalState {
 	return new(mode3)
 }
-func (st *mode2) Duration() int { return mode2Length }
-
-// Mode 2 first 4 ticks
-type mode0ToMode2 struct {
-}
-
-func (st *mode0ToMode2) Init(ppu *PPU) {
-	util.SetBit(&ppu.STAT, 2, 0)
-}
-func (st *mode0ToMode2) Next(_ *PPU) ppuInternalState {
-	return new(mode2)
-}
-func (st *mode0ToMode2) Duration() int { return 4 }
+func (st *mode2ToMode3) Duration() int { return 4 }
