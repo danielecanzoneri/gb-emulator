danielecanzoneri/gb-emulator

commit 6f91b70aafe5d7922d1401b9e3ec865534bce696

Remove automatic recording of game audio (now it is a startup option)

ui/audio.go
@@ -2,12 +2,10 @@ package ui
 
 import (
 	"encoding/binary"
-	"fmt"
+	"github.com/ebitengine/oto/v3"
 	"io"
 	"math"
-	"os"
-
-	"github.com/ebitengine/oto/v3"
+	"path/filepath"
 )
 
 const (
@@ -18,16 +16,12 @@ const (
 	bufferSize = 8192
 )
 
-var AudioFile *os.File
+var (
+	audioFileBuffer         []byte
+	audioFileBufferPosition int
+)
 
 func newAudioPlayer(r io.Reader) (*oto.Player, error) {
-	file, err := os.OpenFile("audio.dat", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		fmt.Println("Error creating audio file:", err)
-	} else {
-		AudioFile = file
-	}
-
 	op := &oto.NewContextOptions{}
 	op.SampleRate = sampleRate
 	op.ChannelCount = channels
@@ -61,6 +55,10 @@ func (ui *UI) Read(buf []byte) (n int, err error) {
 			binary.LittleEndian.PutUint32(buf[bufferPosition:], math.Float32bits(sample))
 			bufferPosition += 4
 
+			// Write also to file buffer
+			binary.LittleEndian.PutUint32(audioFileBuffer[audioFileBufferPosition:], math.Float32bits(sample))
+			audioFileBufferPosition += 4
+
 		default:
 			// If debugger is active and paused, return silence
 			if ui.Paused || (ui.debugger.Active && !ui.debugger.Running) { // If paused, return silence
@@ -87,7 +85,26 @@ func (ui *UI) Read(buf []byte) (n int, err error) {
 			}
 		}
 	}
-	_, _ = AudioFile.Write(buf[:bufferPosition])
+	if ui.audioFile != nil {
+		_, _ = ui.audioFile.Write(audioFileBuffer[:audioFileBufferPosition])
+		audioFileBufferPosition = 0
+	}
 
 	return bufferPosition, nil
+}
+
+func (ui *UI) RecordAudio() (string, error) {
+	// Extract the relative path
+	fileName := filepath.Base(ui.fileName)
+	f, err := createAudioFile(fileName)
+	if err != nil {
+		ui.audioPlayer = nil
+		return "", err
+	}
+
+	// Create buffer
+	audioFileBuffer = make([]byte, bufferSize)
+
+	ui.audioFile = f
+	return f.Name(), nil
 }

main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"flag"
-	"log"
-
 	"github.com/danielecanzoneri/gb-emulator/ui"
+	"log"
 )
 
 var (
 	startWithDebugger = flag.Bool("debug", false, "Start emulator with debugger enabled")
 	bootRom           = flag.String("boot-rom", "boot/bootix_dmg.bin", "Boot ROM filename (\"None\" to skip boot ROM)")
+	recordAudio       = flag.Bool("record", false, "Record game audio (2 channels uncompressed 32-bit float little endian")
 )
 
 func main() {
@@ -31,6 +31,13 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if *recordAudio {
+		if filename, err := gui.RecordAudio(); err != nil {
+			log.Println("Could not record game audio:", err)
+		} else {
+			log.Println("Recording game audio to ", filename)
+		}
+	}
 	if *startWithDebugger {
 		gui.ToggleDebugger()
 	}

ui/file.go
@@ -2,11 +2,14 @@ package ui
 
 import (
 	"errors"
-	"github.com/danielecanzoneri/gb-emulator/gameboy/cartridge"
-	"github.com/sqweek/dialog"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/danielecanzoneri/gb-emulator/gameboy/cartridge"
+	"github.com/sqweek/dialog"
 )
 
 func (ui *UI) LoadNewGame() {
@@ -84,4 +87,20 @@ func getSavFileName(romPath string) string {
 	// Remove gb extension
 	savFile := romPath[:len(romPath)-len(filepath.Ext(romPath))]
 	return savFile + ".sav"
+}
+
+// Create an audio file containing the game audio.
+// The file will have the same name of the game, followed by a timestamp and .dat extension.
+func createAudioFile(romName string) (*os.File, error) {
+	baseName := romName[:len(romName)-len(filepath.Ext(romName))]
+	timestamp := time.Now().Format("20060102_150405")
+	audioFileName := fmt.Sprintf("%s_%s.dat", baseName, timestamp)
+
+	// Create the file
+	file, err := os.Create(audioFileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
 }

ui/ui.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"log"
+	"os"
 
 	"github.com/danielecanzoneri/gb-emulator/ui/debugger"
 
@@ -21,6 +22,7 @@ type UI struct {
 	// Audio player
 	audioBuffer chan float32
 	audioPlayer *oto.Player
+	audioFile   *os.File // Record game audio
 
 	debugString      string
 	debugStringTimer uint

commit a0d6a9f43993f8c1fbbd80f63803f4dc49c755f8

Fix bug introduced in 6f91b70a when playing without recording to a file

ui/audio.go
@@ -56,8 +56,10 @@ func (ui *UI) Read(buf []byte) (n int, err error) {
 			bufferPosition += 4
 
 			// Write also to file buffer
-			binary.LittleEndian.PutUint32(audioFileBuffer[audioFileBufferPosition:], math.Float32bits(sample))
-			audioFileBufferPosition += 4
+			if ui.audioFile != nil {
+				binary.LittleEndian.PutUint32(audioFileBuffer[audioFileBufferPosition:], math.Float32bits(sample))
+				audioFileBufferPosition += 4
+			}
 
 		default:
 			// If debugger is active and paused, return silence